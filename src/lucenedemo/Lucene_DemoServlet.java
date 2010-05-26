19ffa06442890b46bbd3c0c9b3a9b82fd50578b2
Ikai Lan <ikai.lan@gmail.com>
2010-05-25 17:39:46 -0700

    Initial commit

---
+++ src/lucenedemo/Lucene_DemoServlet.java
@@ -0,0 +1,12 @@
+package lucenedemo;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Lucene_DemoServlet extends HttpServlet {
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().println("Hello, world");
+  }
+}
